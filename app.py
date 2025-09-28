0fe69d1a34d4434108e653b4eeb818b16f3a9c78
VISHWANATHAN A <114131407+VishwanathanA@users.noreply.github.com>
2025-09-28 11:33:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from modules import db, Product, Location, ProductMovement
+from sqlalchemy import func
 
 app = Flask(__name__)
 
